6374759e0fa2b79df4dd0fe4c0c94e026f27c176
wiltrouble <wiltrouble@gmail.com>
2019-02-20 14:12:09 -0500

    routes api

---
+++ server/src/index.ts
@@ -0,0 +1,32 @@
+import express, { Application } from 'express';
+
+import indexRoutes from './routes/indexRoutes';
+import gamesRoutes from './routes/gamesRoutes'
+
+class Server {
+
+    public app: Application;
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+
+    config(): void{
+        this.app.set('port', process.env.PORT || 3000);
+    }
+
+    routes(): void{
+        this.app.use(indexRoutes);
+        this.app.use('/api/games', gamesRoutes)
+    }
+
+    start(): void{
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server on port', this.app.get('port'));
+        });
+    }
+}
+
+const server = new Server();
+server.start()
